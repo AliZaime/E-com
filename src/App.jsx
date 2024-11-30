04df2eb18352e8e2e702530a824dc6c90a7d1311
AliZaime <a.zaime@esisa.ac.ma>
2024-11-30 17:19:54 +0100

    nav & smartphones

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar'; // Chemin du fichier Navbar.jsx
-import './App.css'; // Ajoutez un fichier CSS global si nécessaire
-import Smartphones from './Smartphones';
-import "./Smartphones.css"
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
-      <Navbar />
-      <main>
-        <Routes>
-        <Route path="/Smartphones" element={<Smartphones />} />
-        </Routes>
-      </main>
-      
-    </div>
-    </Router>
-    
-  );
 };
 
 export default App;
