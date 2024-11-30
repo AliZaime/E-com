70b5bddcbba267f49dc8a29b9296bf5cf79572b5
Amine <m.a.khatouf@gmail.com>
2024-11-30 17:08:39 +0100

    Inscription_Succes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>nbonjour</h1>
-    </div>
+    <>
+
+    </>
   );
 }
 
---
+++ src/SecondNavbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Second_navbar_style.css'; // Importing the CSS file for styling
+
+function SecondNavbar() {
+    return (
+        <div className="second_navbar">
+            <img src="/LogoTrendify.png" alt="Trendify logo" className="navbar-logo" />
+            <h1 className="navbar-title">Trendify</h1>
+        </div>
+    );
+}
+
+export default SecondNavbar;
