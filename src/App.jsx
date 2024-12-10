bc57a6da0983f53fb073586a4209fc1bcfadf6eb
hamzarais9898 <hmizourais557@gmail.com>
2024-12-10 16:49:32 +0100

    le projet corriger

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar'; // Chemin du fichier Navbar.jsx
-import './App.css'; // Ajoutez un fichier CSS global si nécessaire
+import Navbar from './Navbar';
+import Footer from './Footer';
 import Smartphones from './Smartphones';
 import IPhone from './iPhone';
-import "./Smartphones.css"
-import Footer from './Footer'; // Chemin du fichier Footer.jsx
-import Inscription from './Inscription'; // Chemin du fichier Inscription.jsx
+import Inscription from './Inscription';
 import Connection from './Connection';
-import CustomerTestimonials from "./CustomerTestimonials"
-import CompanyHistory from "./CompanyHistory"
-import Home from "./Home"
-import OurMission from "./OurMission"
+import CustomerTestimonials from './CustomerTestimonials';
+import CompanyHistory from './CompanyHistory';
+import Home from './Home';
+import OurMission from './OurMission';
 import AboutUs from "./AboutUS"
+import './App.css';
 
-function App(){
-    return(
-        <Router>
-        <div className="App">
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        {/* Navbar fixe */}
         <Navbar />
         
+        {/* Contenu principal qui change en fonction de la route */}
         <main>
-            <Home/>
-            <Routes>
-                <Route path="/Smartphones" element={<Smartphones />} />
-                <Route path="/Inscription" element={<Inscription />} />
-                <Route path="/Connection" element={<Connection />} />
-                <Route path="/OurMission" element={< OurMission />} />
-                <Route path="/CompanyHistory" element={< CompanyHistory />} />
-                <Route path="/CustomerTestimonials" element={< CustomerTestimonials />} />
-                <Route path="/AboutUs" element={< AboutUs />} />
-                <Route path="/iPhone" element={< IPhone />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/smartphones" element={<Smartphones />} />
+            <Route path="/smartphones/iphone" element={<IPhone />} />
+            <Route path="/inscription" element={<Inscription />} />
+            <Route path="/connection" element={<Connection />} />
+            <Route path="/ourmission" element={<OurMission />} />
+            <Route path="/companyhistory" element={<CompanyHistory />} />
+            <Route path="/customertestimonials" element={<CustomerTestimonials />} />
+            <Route path="/AboutUs" element={< AboutUs />} />
+          </Routes>
         </main>
         
+        {/* Footer fixe */}
         <Footer />
-        </div>
-        </Router>
-    );
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,9 +3,9 @@ import './Navbar.css'; // Fichier CSS pour styliser la navbar
 import { Link } from 'react-router-dom'; // Importer Link pour les liens de navigation
 
 const Navbar = () => {
-const [showMenu, setShowMenu] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-useEffect(() => {
+  useEffect(() => {
     if (showMenu) {
       document.body.classList.add("blurred");
     } else {
@@ -16,64 +16,62 @@ useEffect(() => {
 
   return (
     <nav className="navbar">
-            <div className="nav1">
-                <div className="navbar-logo">
-                    <img src="/images/TechSpot.png" alt="TechSpot" />
-                    <a  href="/" style={{ fontFamily: "'Protest Revolution', sans-serif" }}>TechSpot</a>
-                </div>
-                <div className="navbar-links">
-                    <a href="/revendre">Revendre</a>
-                    <a href="/notre-pacte-qualite">Notre Pacte Qualité</a>
-                    <a href="/backstories">BackStories</a>
-                    <a href="/aide">Aide</a>
-                </div>
-                <div className="navbar-search">
-                    <input type="text" placeholder="Que cherchez-vous ?" />
-                    <button>🔍</button>
-                </div>
-                <div className="navbar-actions">
-                        <img src="/images/morocco.png" alt="MAR" style={{ width: "40px", height: "25px" }}/>
-                        
-                        <div
-                            id='account'
-                            className="logo-container"
-                            onMouseEnter={() => setShowMenu(true)} // Affiche le menu au survol
-                        >
-                            <img src="/images/person.png" alt="Logo" style={{ width: "25px", height: "25px" }} />
-                            {showMenu && (
-
-                            <div
-                                className="hover-menu"
-                                onMouseEnter={() => setShowMenu(true)} // Empêche la fermeture si la souris est sur le menu
-                                onMouseLeave={() => setShowMenu(false)} // Cache le menu lorsque la souris quitte le menu
-                            >
-                                <button onClick={() => window.location.href = "/Connection"}>Log In</button>
-                                <button onClick={() => window.location.href = "/Inscription"}>Sign In</button>
-                            </div>
-                            )}
-                        </div>
-                    <a href="/">
-                        <img
-                            src="/images/shopping_cart.png"
-                            alt="cart"
-                            style={{ width: "25px", height: "25px" }}
-                        />
-                    </a>
-                </div>  
-            </div>
-            <div className="nav2">
-                <div className="navbar-categories">
-                    <a href="/black-friday" className="highlight">Black Friday</a>
-                    <Link to="/smartphones">Smartphones</Link>
-                    <a href="/ordinateurs-portables">Ordinateurs portables</a>
-                    <a href="/tablettes">Tablettes</a>
-                    <a href="/consoles">Consoles</a>
-                    <a href="/montres-connectees">Montres connectées</a>
-                    <a href="/audio">Audio</a>
-                    <a href="/appareils-electromenagers">Appareils électroménagers</a>
-                    <a href="/autres">Autres</a>
-                </div>
-            </div>   
+      <div className="nav1">
+        <div className="navbar-logo">
+          <img src="/images/TechSpot.png" alt="TechSpot" />
+          <Link to="/" style={{ fontFamily: "'Protest Revolution', sans-serif" }}>TechSpot</Link>
+        </div>
+        <div className="navbar-links">
+          <Link to="/revendre">Revendre</Link>
+          <Link to="/notre-pacte-qualite">Notre Pacte Qualité</Link>
+          <Link to="/backstories">BackStories</Link>
+          <Link to="/aide">Aide</Link>
+        </div>
+        <div className="navbar-search">
+          <input type="text" placeholder="Que cherchez-vous ?" />
+          <button>🔍</button>
+        </div>
+        <div className="navbar-actions">
+          <img src="/images/morocco.png" alt="MAR" style={{ width: "40px", height: "25px" }} />
+          <div
+            id="account"
+            className="logo-container"
+            onMouseEnter={() => setShowMenu(true)}
+          >
+            <img src="/images/person.png" alt="Logo" style={{ width: "25px", height: "25px" }} />
+            {showMenu && (
+              <div
+                className="hover-menu"
+                onMouseEnter={() => setShowMenu(true)}
+                onMouseLeave={() => setShowMenu(false)}
+              >
+                <button onClick={() => (window.location.href = "/Connection")}>Log In</button>
+                <button onClick={() => (window.location.href = "/Inscription")}>Sign In</button>
+              </div>
+            )}
+          </div>
+          <Link to="/">
+            <img
+              src="/images/shopping_cart.png"
+              alt="cart"
+              style={{ width: "25px", height: "25px" }}
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="nav2">
+        <div className="navbar-categories">
+          <Link to="/black-friday" className="highlight">Black Friday</Link>
+          <Link to="/smartphones">Smartphones</Link>
+          <Link to="/ordinateurs-portables">Ordinateurs portables</Link>
+          <Link to="/tablettes">Tablettes</Link>
+          <Link to="/consoles">Consoles</Link>
+          <Link to="/montres-connectees">Montres connectées</Link>
+          <Link to="/audio">Audio</Link>
+          <Link to="/appareils-electromenagers">Appareils électroménagers</Link>
+          <Link to="/autres">Autres</Link>
+        </div>
+      </div>
     </nav>
   );
 };
